acfd0a37e1509aca209f165315a96aac9f58e2ea
rayzen2002 <emanuelzsmj@gmail.com>
2024-01-22 15:26:03 -0300

    adding .env

--- acai/src/lib/api/server.ts
+++ acai/src/lib/api/server.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export async function server() {
   const server = await axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL: process.env.API_HOST,
   })
   return server
 }
