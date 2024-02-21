6d8c86749f0c00475c81e18e0ce81d746cf25431
rayzen2002 <emanuelzsmj@gmail.com>
2024-02-21 19:13:00 -0300

    Testing routes

--- acaiServer/src/routes/get-customer.ts
+++ acaiServer/src/routes/get-customer.ts
@@ -4,27 +4,22 @@ import { prisma } from '../../infra/prisma/database'
 
 export async function getCustomer(server: FastifyInstance) {
   server.get('/customers', { preHandler: [auth] }, async (req, res) => {
-    // const productSchema = z.object({
-    //   name: z.string(),
-    //   description: z.string(),
-    //   priceInCents: z.number(),
-    // })
-    // const product = productSchema.parse(req.body)
-    try {
-      const customersDb = await prisma.customers.findMany()
-      const distributor = await prisma.distributor.findFirst()
-      if (distributor) {
-        const customers = customersDb.map((customer) => {
-          customer.distributorId = distributor.name
-          return customer
-        })
-        return { customers }
-      } else {
-        throw new Error('Sem distribuidores')
-      }
-    } catch (error) {
-      console.error(error)
-      res.status(409)
-    }
+    res.send('Teste')
+    // try {
+    //   const customersDb = await prisma.customers.findMany()
+    //   const distributor = await prisma.distributor.findFirst()
+    //   if (distributor) {
+    //     const customers = customersDb.map((customer) => {
+    //       customer.distributorId = distributor.name
+    //       return customer
+    //     })
+    //     return { customers }
+    //   } else {
+    //     throw new Error('Sem distribuidores')
+    //   }
+    // } catch (error) {
+    //   console.error(error)
+    //   res.status(409)
+    // }
   })
 }
